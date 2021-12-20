ff6e9baf747387f33398d0dae1b1dc260023be5c
wing0221 <pokeanto0221@gmail.com>
2021-12-21 01:16:21 +0900

    get u t

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,139 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+def Setup
+  @user = active_user
+end
+
+  test "name_validation" do
+    # 入力必須
+    user = User.new(email: "test@example.com", password: "password")
+    user.save
+    required_msg = ["名前を入力してください"]  # 追加
+    assert_equal(required_msg, user.errors.full_messages)
+
+    max = 30
+    name = "a" * (max + 1)
+    user.name = name
+    user.save
+    maxlength_msg = ["名前は30文字以内で入力してください"]
+    assert_equal(maxlength_msg, user.errors.full_messages)
+
+    name = "あ" * max
+    user.name = name
+    assert_difference("User.count", 1) do
+      user.save
+    end
+  end
+
+  test "email_validation" do
+    user = User.new(name: "test", password: "password")
+    user.save
+
+    required_msg = ["メールアドレスを入力してください"]  # 追加
+    assert_equal(required_msg, user.errors.full_messages)
+
+    max = 255
+    domain = "@example.com"
+    email = "a" * (max + 1 - domain.length) + domain
+    assert max < email.length
+
+    user.email = email
+    user.save
+    maxlength_msg = ["メールアドレスは#{max}文字以内で入力してください"]
+    assert_equal(maxlength_msg, user.errors.full_messages)
+
+    ok_emails = %w(
+      A@EX.COM
+      a-_@e-x.c-o_m.j_p
+      a.a@ex.com
+      a@e.co.js
+      1.1@ex.com
+      a.a+a@ex.com
+    )
+    ok_emails.each do |email|
+      user.email = email
+      assert user.save
+    end
+
+  end
+
+  test "email_downcase" do
+    # email小文字化テスト
+    email = "USER@EXAMPLE.COM"
+    user = User.new(email: email)
+    user.save
+    assert user.email == email.downcase
+  end
+
+  test "active_user_uniqueness" do
+    email = "test@example.com"
+
+    # アクティブユーザーがいない場合、同じメールアドレスが登録できているか
+    count = 3
+    assert_difference("User.count", count) do
+      count.times do |n|
+        User.create(name: "test", email: email, password: "password")
+      end
+    end
+    active_user.destroy!
+    assert_difference("User.count", 1) do
+      User.create(name: "test", email: email, password: "password", activated: true)
+    end
+    # 一意性は保たれているか
+    assert_equal(1, User.where(email: email, activated: true).count)
+  end
+
+# 追加
+  test "password_validation" do
+    # 入力必須
+    user = User.new(name: "test", email: "test@example.com")
+    user.save
+    required_msg = ["パスワードを入力してください"]
+    assert_equal(required_msg, user.errors.full_messages)
+
+    # min文字以上
+    min = 8
+    user.password = "a" * (min - 1)
+    user.save
+    minlength_msg = ["パスワードは8文字以上で入力してください"]
+    assert_equal(minlength_msg, user.errors.full_messages)
+
+    # max文字以下
+    max = 72
+    user.password = "a" * (max + 1)
+    user.save
+    maxlength_msg = ["パスワードは72文字以内で入力してください"]
+    assert_equal(maxlength_msg, user.errors.full_messages)
+
+    # 書式チェック VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+    ok_passwords = %w(
+      pass---word
+      ________
+      12341234
+      ____pass
+      pass----
+      PASSWORD
+    )
+    ok_passwords.each do |pass|
+      user.password = pass
+      assert user.save
+    end
+
+    ng_passwords = %w(
+      pass/word
+      pass.word
+      |~=?+"a"
+      １２３４５６７８
+      ＡＢＣＤＥＦＧＨ
+      password@
+    )
+    format_msg = ["パスワードは半角英数字•ﾊｲﾌﾝ•ｱﾝﾀﾞｰﾊﾞｰが使えます"]
+    ng_passwords.each do |pass|
+      user.password = pass
+      user.save
+      assert_equal(format_msg, user.errors.full_messages)
+    end
+  end
+
 end
